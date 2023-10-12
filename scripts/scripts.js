cd3e6174282d7b796d162942cfee7ffdd08c9399
AnSannikova <asyadol15@gmail.com>
2023-10-12 20:26:53 +0300

    Добавить диалоговое окно и карточки для карусели

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,41 @@
-const mobileMenuButton = document.querySelector('.header__menu-button');
-const mobileMenu = document.querySelector('.header__menu');
 const page = document.querySelector('.page');
-const backdrop = document.querySelector('.backdrop');
 
-mobileMenuButton.addEventListener('click', function () {
-  mobileMenu.classList.toggle('menu-is-active');
-  page.classList.toggle('menu-is-open');
-});
+function mobileMenuOpen() {
+  const mobileMenuButton = document.querySelector('.header__menu-button');
+  const mobileMenu = document.querySelector('.header__menu');
+  const backdrop = document.querySelector('.backdrop');
 
-backdrop.addEventListener('click', function () {
-  mobileMenu.classList.toggle('menu-is-active');
-  page.classList.toggle('menu-is-open');
-});
+  mobileMenuButton.addEventListener('click', function () {
+    mobileMenu.classList.toggle('menu-is-active');
+    page.classList.toggle('scroll-lock');
+  });
+
+  backdrop.addEventListener('click', function () {
+    mobileMenu.classList.remove('menu-is-active');
+    page.classList.remove('scroll-lock');
+  });
+}
+mobileMenuOpen();
+
+function showModalWindow() {
+  const buttonShowModal = document.querySelectorAll('.button_show-modal');
+  const dialog = document.getElementById('dialog');
+  const buttonCloseModal = document.querySelector('.dialog__close-button');
+
+  buttonShowModal.forEach((item) => {
+    item.addEventListener('click', function () {
+      dialog.showModal();
+      page.classList.add('scroll-lock');
+    });
+  });
+
+  buttonCloseModal.addEventListener('click', function () {
+    dialog.close();
+  });
+
+  dialog.addEventListener('close', function () {
+    page.classList.remove('scroll-lock');
+  });
+}
+
+showModalWindow();
