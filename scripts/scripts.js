6008cd664deb48ec7b240c10067943744007fd33
AnSannikova <asyadol15@gmail.com>
2023-10-21 12:21:54 +0300

    Добавить закрытие диалога при клике на подложку

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,41 +1,51 @@
 const page = document.querySelector('.page');
 
 function mobileMenuOpen() {
-  const mobileMenuButton = document.querySelector('.header__menu-button');
-  const mobileMenu = document.querySelector('.header__menu');
-  const backdrop = document.querySelector('.backdrop');
+  const mobileMenuButton = page.querySelector('.header__menu-button');
+  const mobileMenu = page.querySelector('.header__menu');
+  const backdrop = page.querySelector('.backdrop');
 
-  mobileMenuButton.addEventListener('click', function () {
+  mobileMenuButton.addEventListener('click', () => {
+    mobileMenuButton.classList.toggle('button-change');
     mobileMenu.classList.toggle('menu-is-active');
     page.classList.toggle('scroll-lock');
   });
 
-  backdrop.addEventListener('click', function () {
+  backdrop.addEventListener('click', () => {
     mobileMenu.classList.remove('menu-is-active');
     page.classList.remove('scroll-lock');
   });
 }
 mobileMenuOpen();
 
+
 function showModalWindow() {
   const buttonShowModal = document.querySelectorAll('.button_show-modal');
-  const dialog = document.getElementById('dialog');
-  const buttonCloseModal = document.querySelector('.dialog__close-button');
+  const modalWindow = document.getElementById('dialog');
+  const buttonCloseModal = dialog.querySelector('.dialog__close-button');
 
   buttonShowModal.forEach((item) => {
-    item.addEventListener('click', function () {
-      dialog.showModal();
+    item.addEventListener('click', () => {
+      modalWindow.showModal();
       page.classList.add('scroll-lock');
     });
   });
 
-  buttonCloseModal.addEventListener('click', function () {
-    dialog.close();
+  buttonCloseModal.addEventListener('click', () => {
+    modalWindow.close();
   });
 
-  dialog.addEventListener('close', function () {
+  modalWindow.addEventListener('close', () => {
     page.classList.remove('scroll-lock');
   });
-}
 
+  modalWindow.addEventListener("click", (evt) => {
+    const modalWindow = evt.currentTarget;
+    const backDrop = evt.target;
+
+    if (backDrop === modalWindow) {
+      modalWindow.close();
+    }
+  })
+}
 showModalWindow();
